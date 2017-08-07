666336c2b8b2fe8bb5508975ff79b5aecbf7bdf2
Dakota Nichols <dakotachantz@gmail.com>
2017-08-07 15:54:54 -0500

    added all styling

---
+++ main.js
@@ -0,0 +1,39 @@
+let req = new XMLHttpRequest();
+req.open("GET", "https://api.github.com/users/dakotachantz");
+req.addEventListener("load", reqListener);
+req.send();
+
+function reqListener() {
+    let data = JSON.parse(this.responseText);
+
+    const heading = `
+    <h1>${data.name}</h1>
+`
+    const basics =
+        `
+    <h2>The Basics</h2>
+    <div class="basics_content">
+    <p>Name: ${data.name}</p>
+    <p>Github URL: <a href="${data.html_url}">${data.login}</a></p>
+    <p>Email: ${data.email}</p>
+    <p>Company: ${data.company}</p>
+    <p>Website: <a href="${data.blog}">${data.blog}</a></p>
+    </div>
+`
+
+    const story =
+        `
+ <h2>The Story</h2>
+<div class="story_content">
+<p>${data.bio}</p>
+    <img src="${data.avatar_url}" alt="avatar picture">
+    </div>
+ `
+    document.querySelector("header").innerHTML = heading;
+    document.querySelector(".basics").innerHTML = basics;
+    document.querySelector(".story").innerHTML = story;
+}
+
+
+
+
